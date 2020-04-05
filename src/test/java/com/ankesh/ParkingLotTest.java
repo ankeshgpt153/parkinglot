819d153859eaf4f09008e0ce84c95b1b732eaee8
ankesh <ankeshgpt153@gmail.com>
2020-04-05 17:28:25 +0530

    junit test for parking lot full,capacity and empty

--- src/test/java/com/ankesh/ParkingLotTest.java
+++ src/test/java/com/ankesh/ParkingLotTest.java
@@ -2,8 +2,10 @@ package com.ankesh;
 
 import com.ankesh.exception.ErrorCode;
 import com.ankesh.exception.ParkingException;
+import com.ankesh.model.Car;
 import com.ankesh.service.ParkingService;
 import com.ankesh.service.impl.ParkingServiceImpl;
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -14,6 +16,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -23,6 +26,7 @@ public class ParkingLotTest {
 
     private static final String CREATED_PARKING_LOT_MESSAGE = "createdparkinglotwith65slots";
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    String content=outContent.toString().trim().replace(" ", "");
 
     ParkingService instance = new ParkingServiceImpl();
 
@@ -69,4 +73,55 @@ public class ParkingLotTest {
         instance.createParkingLot(65);
         instance.doCleanup();
     }
+
+
+    @Test(expected = ParkingException.class)
+    public void testParkingCapacity() throws Exception {
+
+        thrown.expectMessage(CoreMatchers.is(ErrorCode.PARKING_NOT_EXIST_ERROR.getMessage()));
+
+        instance.park(new Car("KA-01-HH-1234", "White"));
+        assertEquals("Sorry,CarParkingDoesnotExist", content);
+
+        instance.createParkingLot(11);
+        instance.park(new Car("KA-01-HH-1234", "White"));
+        instance.park(new Car("KA-01-HH-9999", "White"));
+        instance.park(new Car("KA-01-BB-0001", "Black"));
+
+        assertEquals(3, instance.getAvailableSlotsCount());
+        instance.doCleanup();
+    }
+
+    @Test
+    public void testEmptyParkingLot() throws Exception {
+        ParkingService instance = new ParkingServiceImpl();
+        thrown.expect(ParkingException.class);
+        thrown.expectMessage(CoreMatchers.is(ErrorCode.PARKING_NOT_EXIST_ERROR.getMessage()));
+        instance.getStatus();
+        assertTrue("Sorry,CarParkingDoesnotExist".equalsIgnoreCase(content));
+        instance.createParkingLot(6);
+        instance.getStatus();
+        assertTrue(
+                "Sorry,CarParkingDoesnotExist\ncreatedparkinglotwith6slots\nSlotNo.\tRegistrationNo.\tColor\nSorry,parkinglotisempty."
+                        .equalsIgnoreCase(content));
+        instance.doCleanup();
+    }
+
+    @Test
+    public void testParkingLotIsFull() throws Exception {
+        ParkingService instance = new ParkingServiceImpl();
+        thrown.expect(ParkingException.class);
+        thrown.expectMessage(CoreMatchers.is(ErrorCode.PARKING_NOT_EXIST_ERROR.getMessage()));
+        instance.park(new Car("KA-01-HH-1234", "White"));
+        assertEquals("Sorry,CarParkingDoesnotExist", content);
+        instance.createParkingLot(2);
+        instance.park(new Car("KA-01-HH-1234", "White"));
+        instance.park(new Car("KA-01-HH-9999", "White"));
+        instance.park(new Car("KA-01-BB-0001", "Black"));
+        assertTrue("createdparkinglotwith2slots\\nAllocatedslotnumber:1\nAllocatedslotnumber:2\nSorry,parkinglotisfull"
+                .equalsIgnoreCase(content));
+        instance.doCleanup();
+    }
+
+
 }
